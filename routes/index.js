9429c33e14739af98a4a475ec72731c05df069e0
amitrke <amitrke@gmail.com>
2022-03-15 00:57:20 +0000

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/version', function(req, res, next) {
+  res.json({"version": 1.0});
+});
+
 module.exports = router;
